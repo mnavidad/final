5c3a30ee74e3623caf583e622d49305410a6dcea
Michelle Christmas <mnavidad@users.noreply.github.com>
2019-05-12 21:19:52 -0400

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,7 +14,91 @@ function makeWords() {
 			text: "jquery",
 			weight: 12
 		}, {
-			text: "programming",
+			text: "programming",const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "DS Math Skills",
+				weight: 12.3
+			}, {
+				text: "Probability and Data",
+				weight: 8
+			}, {
+				text: "Inferencial Stats",
+				weight: 14
+			}, {
+				text: "Linear Regression",
+				weight: 13
+			}, {
+				text: "Bayesian Stats",
+				weight: 7
+			}, {
+				text: "ML with Python",
+				weight: 15
+			}, {
+				text: "Linear Algebra",
+				weight: 9
+			}, {
+				text: "Multivariate Calculus",
+				weight: 10
+			}, {
+				text: "ML PCA",
+				weight: 9
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Full-Stack Web Developer.", "love everything about GIS code.", "also learn everything about spatial data.", "but most of all spatial problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
+
 			weight: 10
 		}, {
 			text: "python",
@@ -46,7 +130,91 @@ function makeWords() {
 		}, {
 			text: "numpy",
 			weight: 7
-		}, {
+		}, {const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "DS Math Skills",
+				weight: 12.3
+			}, {
+				text: "Probability and Data",
+				weight: 8
+			}, {
+				text: "Inferencial Stats",
+				weight: 14
+			}, {
+				text: "Linear Regression",
+				weight: 13
+			}, {
+				text: "Bayesian Stats",
+				weight: 7
+			}, {
+				text: "ML with Python",
+				weight: 15
+			}, {
+				text: "Linear Algebra",
+				weight: 9
+			}, {
+				text: "Multivariate Calculus",
+				weight: 10
+			}, {
+				text: "ML PCA",
+				weight: 9
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Full-Stack Web Developer.", "love everything about GIS code.", "also learn everything about spatial data.", "but most of all spatial problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
+
 			text: "matplotlib",
 			weight: 7
 		}, {
